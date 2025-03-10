e409d2d6e62cbe0995e2c626b2409ddd25f1b781
mo <1maalinma@gmail.com>
2025-03-10 10:54:11 -0400

    add frame, delete frame, other changes

--- next/components/Canvas.tsx
+++ next/components/Canvas.tsx
@@ -1,30 +1,69 @@
 // client-side rendered
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useCallback, useRef, useState } from "react";
 
 export default function Canvas() {
 	// update canvas without rerender -> useref
 	const canvasRef = useRef<HTMLCanvasElement>(null);
 	const [isDrawing, setIsDrawing] = useState(false);
-	const [frames, setFrames] = useState<string[]>([]);
+	const [frames, setFrames] = useState<string[]>([""]);
 	const [currentFrame, setCurrentFrame] = useState<number>(0);
+	const [isPlaying, setIsPlaying] = useState(false);
+	const intervalRef = useRef<NodeJS.Timeout | null>(null);
+	const [frameRate, setFrameRate] = useState<number>(24);
+	// update frame using current data on canvas
+	const updateFrame = useCallback(
+		() => {
+			const canvas = canvasRef.current;
+			if (!canvas) return;
+			// read the curr frame
+			const imageData = canvas.toDataURL();
+
+			setFrames((prevFrames) => {
+				const newFrames = [...prevFrames]; 
+				newFrames[currentFrame] = imageData;
+				return newFrames;
+			});
+		}, [currentFrame]
+	);
+
+	// load frame from base64
+	const loadFrame = useCallback(
+		(index: number) => {
+			const canvas = canvasRef.current;
+			if (!canvas) return;
+
+			const ctx = canvas.getContext("2d");
+			const image = new Image();
+			image.src = frames[index] || ""; 
+
+			// use callback to draw from array
+			image.onload = () => {
+				// optional chain
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				ctx.drawImage(image, 0, 0);
+			};
+		}, [frames]
+	);
 
 	useEffect(() => {
+		// canvas context allows us to manipulate
 		const canvas = canvasRef.current;
 		if (!canvas) return;
-		// canvas context allows us to manipulate
 		// the canvas
 		const ctx = canvas.getContext("2d");
 		if (!ctx) return;
 
-		// modify context
+		// modify context to get a more pixel
 		ctx.lineWidth = 5;
-		ctx.lineCap = "round";
+		ctx.lineCap = "square";
+		ctx.lineJoin = "miter";
 		ctx.strokeStyle = "black";
 
-		// handlers to begin a path, follow
-		// and close path. secret sauce
+		loadFrame(currentFrame);
+
+		// handlers to draw 
 		const startDrawing = (e: MouseEvent) => {
 			ctx.beginPath();
 			ctx.moveTo(e.offsetX, e.offsetY);
@@ -40,33 +79,45 @@ export default function Canvas() {
 		const stopDrawing = () => {
 			setIsDrawing(false);
 			ctx.closePath();
-			captureFrame();
-		};
-
-		const captureFrame = () => {
-			const canvas = canvasRef.current;
-			if (!canvas) return;
-
-			// base64 "image"
-			const imageData = canvas.toDataURL();
-			setFrames((prevFrames) => [...prevFrames.slice(0, currentFrame + 1), imageData]);
-			setCurrentFrame((prev) => prev + 1);
+			updateFrame();
 		};
 
 		// apply handlers
 		canvas.addEventListener("mousedown", startDrawing);
 		canvas.addEventListener("mousemove", draw);
 		canvas.addEventListener("mouseup", stopDrawing);
-		canvas.addEventListener("mouseleave", stopDrawing);
 
 		// when we return erase handlers
 		return () => {
 			canvas.removeEventListener("mousedown", startDrawing);
 			canvas.removeEventListener("mousemove", draw);
 			canvas.removeEventListener("mouseup", stopDrawing);
-			canvas.removeEventListener("mouseleave", stopDrawing);
 		};
-	}, [isDrawing, currentFrame]);
+	}, [isDrawing, currentFrame, loadFrame, updateFrame]);
+
+
+	// create a current frame with a splice
+	const addNewFrame = () => {
+		setFrames((prevFrames) => {
+			const newFrames = [...prevFrames];
+			newFrames.splice(currentFrame + 1, 0, "");
+			return newFrames;
+		});
+
+		setCurrentFrame(currentFrame + 1);
+	};
+
+	const deleteFrame = () => {
+		if (frames.length == 1) return;
+
+		setFrames((prevFrames) => {
+			const newFrames = prevFrames.filter((_, index) => index != currentFrame);	
+			return newFrames;	
+		});
+
+
+		setCurrentFrame(Math.max(currentFrame - 1, 0));
+	};
 
 	// use loadFrame and setCurrentFrame handlers
 	const goToPreviousFrame = () => {
@@ -84,40 +135,49 @@ export default function Canvas() {
 		}
 	}
 
-	const loadFrame = (index: number) => {
-		const canvas = canvasRef.current;
-		if (!canvas) return;
-		const ctx = canvas.getContext("2d");
-		// whattt
-		const image = new Image();
-		image.src = frames[index];
-
-		// use callback to draw from array
-		image.onload = () => {
-			// optional chain
-			ctx?.clearRect(0, 0, canvas.width, canvas.height);
-			ctx?.drawImage(image, 0, 0);
-		};
-	};
 
 	const playFrames = () => {
-		let i = 0;
-		const N = frames.length;
-		setInterval(() => {
-			loadFrame(i % N);
-			setCurrentFrame(i % N);
-			i++;
-		}, 1000 / 24); // 24 frames per second
+		if (isPlaying) {
+			if (intervalRef.current) {
+				clearInterval(intervalRef.current);
+				intervalRef.current = null;
+			}
+			setIsPlaying(false);
+		} 
+		else {
+			let i = 0;
+			const N = frames.length;
+			intervalRef.current = setInterval(() => {
+				loadFrame(i % N);
+				setCurrentFrame(i % N);
+				i++;
+			}, 1000 / frameRate); // 24 frames per second
+
+			setIsPlaying(true);
+		}
 	};
 
-	// return div with canvas
+	// use the frameRate slider
+	const handleFrameRate = (value: number) => {
+		setFrameRate(value);
+		console.log(frameRate);
+	};
+
+	// return div with canvas and a few buttons and inputs
 	return (
 		<div className="flex justify-center items-center h-screen bg-gray-100">
-			<canvas ref={canvasRef} width={800} height={500} className="border bg-white" />
+			<canvas ref={canvasRef} width={800} height={500} className="border bg-white"  />
+			<p className="px-3 py-3">{frameRate}</p>
+			<p className="px-3 py-3">{1 + currentFrame}</p>
+
 			<div className="mt-4">
-				<button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={goToPreviousFrame}>prev</button>
-				<button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={goToNextFrame}>next</button>
-				<button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={playFrames}>play</button>
+				<input id="frameRateSelector" className="block mb-2" type="range" min={12} max={60} value={frameRate} onChange={(e) => handleFrameRate(e.target.value)}/>
+				<button className="bg-green-500 text-white px-4 py-3 rounded" onClick={addNewFrame}>new frame</button>
+				<button className="bg-red-500 text-white px-4 py-3 rounded" onClick={deleteFrame}>delete frame</button>
+
+				<button className="bg-blue-500 text-white px-4 py-3 rounded" onClick={goToPreviousFrame}>prev</button>
+				<button className="bg-blue-500 text-white px-4 py-3 rounded" onClick={goToNextFrame}>next</button>
+				<button className="bg-blue-500 text-white px-4 py-3 rounded" onClick={playFrames}>{isPlaying ? "stop" : "play" }</button>
 			</div>
 		</div>
 	);
